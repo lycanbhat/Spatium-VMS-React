8fbc05b454b9dbefd1dc17314b0983a3b263a224
iamarjunsk <dev.arjunsk@gmail.com>
2024-06-01 22:08:00 +0530

    close modal removed while clicking outside

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,7 +6,9 @@ export default function Modal({children,title,closeM}) {
     e.stopPropagation()
   }
   return (
-    <div className="absolute z-30 overflow-y-auto top-0 left-0 w-full h-screen bg-[#00000064] flex items-center" onClick={closeM}>
+    <div className="absolute z-30 overflow-y-auto top-0 left-0 w-full h-screen bg-[#00000064] flex items-center" 
+    // onClick={closeM}
+    >
         <div className="bg-white max-w-[70%] mx-auto" onClick={preventClose}>
             <header className="px-6 h-12 flex items-center text-dark-400 text-xl font-medium justify-between border-b">{title} <img className="w-3 cursor-pointer" onClick={closeM} src={closeIcon} alt="" /></header>
             <section className="py-3">
